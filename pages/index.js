3c99ed2e97aad9f6b665826fc16ea0c2c55e83e2
frozar <fabien.rozar@gmail.com>
2020-12-05 13:50:01 +0400

    Github banner.

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,15 @@
 // import Head from "next/head";
 // import styles from "../styles/Home.module.css";
 import Stretcher from "./main";
+import GithubCorner from "react-github-corner";
 
 export default function Home() {
   return (
     <>
-      <div>Home</div>
+      <GithubCorner
+        href="https://github.com/frozar/superporewered-next"
+        bannerColor="#30E89F"
+      />
       <Stretcher />
     </>
   );
